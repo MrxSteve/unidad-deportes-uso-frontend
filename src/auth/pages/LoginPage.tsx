f514f91fae471871e93ad2d76dee2b4d02fd86c4
ismaelGarcia23 <ismaelhernan1237@gmail.com>
2026-01-29 01:53:12 -0600

    Ajuste de dimensiones

--- src/auth/pages/LoginPage.tsx
+++ src/auth/pages/LoginPage.tsx
@@ -9,7 +9,7 @@ export default function LoginPage() {
   return (
     <div className="h-screen  w-full flex items-center justify-center bg-gradient-to-br from-blue-600 to-green-300 p-4 overflow-hidden">
       
-      <div className=" min-h-[700px] flex w-full max-w-4xl bg-white rounded-3xl shadow-2xl overflow-hidden shadow-black/20 ">
+      <div className=" min-h-full flex w-full max-w-4xl bg-white rounded-3xl shadow-2xl overflow-hidden shadow-black/20 ">
         
         <div className="w-full lg:w-1/2 p-8 sm:p-12 flex flex-col justify-center items-center bg-white">
           {/* Reducimos el max-w de sm a xs para que los botones y cuadros sean más cortos */}
